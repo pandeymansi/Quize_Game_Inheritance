c44f2b851390e7fc21b8144727da0f0b236240a8
Mansi Pande <156072272+pandeymansi@users.noreply.github.com>
2024-07-23 18:09:58 +0530

    Q&Options
    This is a user-defined module which contains few questions and options for the quiz. The questions are from the Inheritance concept of Python Programming.

---
+++ quiz_database.py
@@ -0,0 +1,15 @@
+# A list which consist dictionary within it (our questions are assigned to the dictionary)
+question_bank = [
+    {"text": "The ability of one class to acquire methods and attributes from another class is called _ _ _ _.",
+     "answer": "A"},
+    {"text": "Which of the following is a type of inheritance?", "answer": "D"},
+    {"text": "What type of inheritance has multiple subclass to a single superclass?", "answer": "C"},
+    {"text": "What is the depth of multilevel inheritance in Python?", "answer": "C"},
+    {"text": "What does MRO stand for?", "answer": "B"}                              
+    ]
+# A list enclosed within a list (Options are assigned here)
+options = [["A. Inheritance", "B. Abstraction", "C. Polymorphism", "D. Objects"],
+          ["A. Single", "B. Double", "C. Multiple", "D. both A and C"],
+          ["A. Multiple Inheritance", "B. Multilevel Inheritance", "C. Hierarchical Inheritance", "D. None of these"],
+          ["A. Two level", "B. Three level", "B. Any level", "D. None of these"],
+          ["A. Method Recursive Object", "B. Method Resolution Order", "C. Main Resolution Order", "D. Method Resemble Order"]]
